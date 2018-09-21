83d3897ff91bc582d136e326190b402c6ccd7df1
jackson29 <jacksonssantos@bol.com.br>
2018-09-21 13:36:19 -0300

    codigonovo

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
-<?php
-echo "Hello World"
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="utf-8">
+   <link rel="stylesheet" type="text/css" href="estilo.css">
+	 <title></title>
+</head>
+<body>
+<div id="tudo">
+	<div id="cabecalho">
+		<h1>BiblioTéc</h1>
+</div>
+<div id="menu">
+	<h2> <a href="">Home</h2>
+	<h2> <a href="">Sobre</h2>
+	<h2> <a href="">Motivos</h2>
+	<h2> <a href="">Desenvolvedores</h2>
+</div>
+<div id="conteudo">
+
+</div>
+</body>
+</html>
