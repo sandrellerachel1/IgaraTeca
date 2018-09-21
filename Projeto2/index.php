02007553118012e7a626c1ddb2af27f0f2c7a2db
jackson29 <jacksonssantos@bol.com.br>
2018-09-21 14:51:13 -0300

    att

---
+++ Projeto2/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Biblioteca IG</title>
+	<link rel="stylesheet" type="text/css" href="css.css">
+</head>
+<body>
+	<div id="menu">
+
+		<ul>
+			<strong>
+				<li><a href="#">Início</a></li>
+				<li><a href="#">Livros</a></li>
+				<li><a href="motivo.php">Motivos</a></li>
+				<li><a href="#">Sobre</a></li>
+				<li><a href="#">Desenvolvedores</a></li>
+			</strong>
+		</ul>
+	</div>
+</body>
+</html>
---
+++ Projeto2/motivo.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Motivos</title>
+	<link rel="stylesheet" type="text/css" href="css.css">
+</head>
+<body>
+	<div id="menu">
+		<ul>
+			<strong>
+				<li><a href="index.php">Início</a></li>
+				<li><a href="#">Livros</a></li>
+				<li><a href="motivo.php">Motivos</a></li>
+				<li><a href="#">Sobre</a></li>
+				<li><a href="#">Desenvolvedores</a></li>
+			</strong>
+		</ul>
+	</div>
+	<div id="mot"><center>
+		<p>GAD – Biblio</p><br>
+		<p> Professor: Alexandre Strapação Guedes Vianna</p>
+		<p>	Grupo: Ricardo Maranhão, Rivaldo Valle, Jackson da Silva, Sandrelle Rachel, Alessandro José, José Augusto, Raquel Gabrielly.
+		</p><br>
+
+		<p> Criar um sistema automático que gerencie bibliotecas.
+			O Sistema-de-gestão-para-bibliotecas gerencia o acervo de livros, DVD's, Blue-Ray’s e periódicos, permitindo cadastrar os empréstimos, devoluções e as baixas. E através da Internet é possível a consulta ao acervo listados por obra, aluno e outros parâmetros e a solicitação de reservas.
+		</p><br>
+			
+		<p> Principais funcionalidades:</p><br>
+			Quanto ao Acervo: Cadastro de livros, DVS's da biblioteca, com etiquetas de código de barras, facilitando o empréstimo e devolução dos mesmos.
+			Quanto aos empréstimos, devoluções e reservas: Gerencia os empréstimos e as devoluções dos exemplares com emissão de comprovantes por e-mail. Determina a data de devolução prevista. Permitindo restringir os empréstimos para os usuários com multa. Calcula automaticamente multas a serem cobradas. Gerencia as reservas por obra, a solicitação pode ser feita pela Internet e o sistema emite e-mail notificando que o livro já está disponível.
+		</p><br>
+	</center></div>
+</body>
+</html>
